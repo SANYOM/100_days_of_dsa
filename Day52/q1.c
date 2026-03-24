2cbe439f55629b735621b210f8ab051293d2b3b6
SANYOM <sanyomthareja@gmail.com>
2026-03-24 10:13:34 +0530

    Day52

---
+++ Day52/q1.c
@@ -0,0 +1,85 @@
+/*
+Problem Statement:
+Find the Lowest Common Ancestor (LCA) of two nodes in a Binary Tree.
+
+Input Format:
+- First line contains integer N
+- Second line contains level-order traversal (-1 represents NULL)
+- Third line contains two node values
+
+Output Format:
+- Print the LCA value
+
+Example:
+Input:
+7
+1 2 3 4 5 6 7
+4 5
+
+Output:
+2
+*/
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int data;
+    struct Node*left;
+    struct Node*right;
+};
+struct Node*createNode(int value)
+{
+    struct Node*newNode=(struct Node*)malloc(sizeof(struct Node));
+    newNode->data=value;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+}
+struct Node*buildTree(int arr[],int n)
+{
+    if(n==0||arr[0]==-1) return NULL;
+    struct Node**nodes=(struct Node**)malloc(n*sizeof(struct Node*));
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==-1) nodes[i]=NULL;
+        else nodes[i]=createNode(arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(nodes[i]!=NULL)
+        {
+            int leftIndex=2*i+1;
+            int rightIndex=2*i+2;
+            if(leftIndex<n) nodes[i]->left=nodes[leftIndex];
+            if(rightIndex<n) nodes[i]->right=nodes[rightIndex];
+        }
+    }
+    struct Node*root=nodes[0];
+    free(nodes);
+    return root;
+}
+struct Node*findLCA(struct Node*root,int n1,int n2)
+{
+    if(root==NULL) return NULL;
+    if(root->data==n1||root->data==n2) return root;
+    struct Node*leftLCA=findLCA(root->left,n1,n2);
+    struct Node*rightLCA=findLCA(root->right,n1,n2);
+    if(leftLCA&&rightLCA) return root;
+    return (leftLCA!=NULL)?leftLCA:rightLCA;
+}
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for(int i=0;i<N;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int n1,n2;
+    scanf("%d %d",&n1,&n2);
+    struct Node*root=buildTree(arr,N);
+    struct Node*lca=findLCA(root,n1,n2);
+    if(lca!=NULL) printf("%d\n",lca->data);
+    return 0;
+}
